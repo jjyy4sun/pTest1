793e4de9da99cf90fb4239e01606cd78a22c7661
Unbuntu_sun <suntian0@gmail.com>
2016-04-25 22:14:22 +0800

    util

--- Markup/util.py
+++ Markup/util.py
@@ -1,1 +1,11 @@
-def line 
+def lines(file):
+	for line in file:
+		yield line
+def blocks(file):
+	block = []
+	for line in lines(file):
+		if line.strip():
+			block.append(line)
+		elif block:
+			yield ''.join(block).strip()
+			block = []
